3b8653e4b5e125490a0d1deb62cfd88373598130
Seiei Higa <hanachin@gmail.com>
2013-01-13 08:32:28 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SnapTwitter::Application.config.secret_token = 'd86dd7c3774136b9face3f9d4ed349686ef099ef57232c79da655b545ffd512a53da1f4a604f744a56e8ff797b58a4ae03f190f31109f5a74c4eb98a4efb1f21'
